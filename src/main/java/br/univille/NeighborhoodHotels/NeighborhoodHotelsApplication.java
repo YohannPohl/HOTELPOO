baf268b8608bfbcb628e7315ce326df7d04515f2
MATHEUS DE ARAÚJO <matheusaraujo@univille.br>
2024-10-30 20:03:30 -0300

    initial Spring

---
+++ src/main/java/br/univille/NeighborhoodHotels/NeighborhoodHotelsApplication.java
@@ -0,0 +1,13 @@
+package br.univille.NeighborhoodHotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborhoodHotelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborhoodHotelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/NeighborhoodHotels/NeighborhoodHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.NeighborhoodHotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighborhoodHotelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
